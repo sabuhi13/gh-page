49e6724b2a2bd1c0445f01a6b216caf3182ef1fe
sabuhi13 <sabuhi.a@al.ventures>
2022-08-10 17:20:51 +0400

    save

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://raw.githubusercontent.com/sabuhi13/doc/main/docs/index.md').then( res => {
-        document.getElementById('docs').innerHTML = res;
-    });
+    const con = await fetch('https://raw.githubusercontent.com/sabuhi13/doc/main/docs/index.md');
+    const response = await con.text();
+    
+    document.getElementById('docs').innerHTML = response;
 }, false);
