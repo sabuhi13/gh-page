787620e16489aedd8419878c706f78ba5726dca0
sabuhi13 <sabuhi.a@al.ventures>
2022-08-10 17:30:46 +0400

    t

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener('DOMContentLoaded', async function() {
     const con = await fetch('https://raw.githubusercontent.com/sabuhi13/doc/main/docs/index.md');
     const response = await con.text();
     
