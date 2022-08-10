5685c4af7162eb9428d13a36d8e009ca21bb64c6
sabuhi13 <sabuhi.a@al.ventures>
2022-08-10 17:36:23 +0400

    fix

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@ window.addEventListener('DOMContentLoaded', async function() {
     const con = await fetch('https://raw.githubusercontent.com/sabuhi13/doc/main/docs/index.md');
     const response = await con.text();
     
-    document.getElementById('docs').innerHTML = response;
+    document.getElementById('docs').innerHTML = marked.parse(response);
 }, false);
+
+/**
+ * https://raw.githubusercontent.com/sabuhi13/doc/main/docs/index.md
+ * 
+ * https://api.github.com/repos/laravel/laravel
+ */
